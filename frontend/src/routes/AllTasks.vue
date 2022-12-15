c563eab3c0edc2ac1b08b70b19f30367c7f69b08
Doug Saylor <doug@saylorsolutions.com>
2022-12-15 01:44:37 -0600

    Adding done state mutations through the UI
     - The status in the tasks view is now clickable.
     - Done tasks will implicitly sort under not done tasks.
     - Separating out the sort method so it's easier to reference and
       update.
     - Adding fg color vars for good/bad cues.

--- frontend/src/routes/AllTasks.vue
+++ frontend/src/routes/AllTasks.vue
@@ -1,40 +1,43 @@
 <template>
-  <loading v-if="isLoading"></loading>
-  <div v-else class="content">
-    <button id="new-task" @click.stop.prevent="this.newTasksShown = true">New Task</button>
-    <div class="table">
-      <div class="row header">
-        <p>ID</p>
-        <p>Task Name</p>
-        <p>Description</p>
-        <p>Status</p>
+  <keep-alive>
+    <div class="content">
+      <loading v-show="isLoading"></loading>
+      <button v-show="!isLoading" id="new-task" @click.stop.prevent="this.newTasksShown = true">New Task</button>
+      <div v-show="!isLoading" class="table">
+        <div class="row header">
+          <p>ID</p>
+          <p>Task Name</p>
+          <p>Description</p>
+          <p>Status</p>
+        </div>
+        <div v-if="noTasks" class="row empty">
+          <td colspan="4"><h3>No tasks found</h3></td>
+        </div>
+        <div v-else v-for="task in tasks" :key="task.id" class="row">
+          <p :class="task.done ? 'done': ''">{{ task.id }}</p>
+          <p :class="task.done ? 'done': ''">{{ task.name }}</p>
+          <p :class="task.done ? 'done': ''">{{ task.description }}</p>
+          <p :class="task.done ? 'done': ''" @click="toggleTaskDone(task.id)">{{ task.done ? "Done" : "Not Done" }}</p>
+        </div>
       </div>
-      <div v-if="noTasks" class="row empty">
-        <td colspan="4"><h3>No tasks found</h3></td>
-      </div>
-      <div v-else v-for="task in tasks" :key="task.id" class="row">
-        <p>{{task.id}}</p>
-        <p>{{ task.name }}</p>
-        <p>{{ task.description }}</p>
-        <p>{{ task.done ? "Done" : "Not Done" }}</p>
-      </div>
+      <modal v-show="newTasksShown" @dialogClose="toggleNewTasks" title="Create Task">
+        <create-task @clickedCancel="toggleNewTasks" @taskCreated="addNewTask"></create-task>
+      </modal>
     </div>
-  </div>
-  <modal v-show="newTasksShown" @dialogClose="toggleNewTasks" title="Create Task">
-    <create-task @clickedCancel="toggleNewTasks" @taskCreated="addNewTask"></create-task>
-  </modal>
+  </keep-alive>
 </template>
 
 <script>
+import {KeepAlive} from "vue";
 import loadState from "../loadState.js";
 import Loading from "../components/Loading.vue";
-import {GetAllTasks} from "../wailsjs/go/main/TaskController.js";
+import {GetAllTasks, UpdateTask} from "../wailsjs/go/main/TaskController.js";
 import Modal from "../components/Modal.vue";
 import CreateTask from "../components/CreateTask.vue";
 
 export default {
   name: "AllTasks",
-  components: {Loading, Modal, CreateTask},
+  components: {Loading, Modal, CreateTask, KeepAlive},
   mixins: [loadState],
   data: () => {
     return {
@@ -47,14 +50,8 @@ export default {
       this.startLoading();
       GetAllTasks()
           .then(tasks => {
-            this.tasks = tasks.sort((a, b) => {
-              if (a.id < b.id) {
-                return -1;
-              } else if (a.id > b.id) {
-                return 1;
-              }
-              return 0;
-            });
+            this.tasks = tasks;
+            this.sortTasks();
           })
           .catch(console.error)
           .then(() => {
@@ -65,12 +62,44 @@ export default {
       this.newTasksShown = !this.newTasksShown
     },
     addNewTask(data) {
-      if(data) {
+      if (data) {
         if (!this.tasks) {
           this.tasks = [];
         }
         this.tasks.push(data);
       }
+    },
+    toggleTaskDone(id) {
+      this.tasks.forEach((item, i) => {
+        if (item.id === id) {
+          item.done = !item.done;
+          this.startLoading();
+          UpdateTask(id, item)
+              .then(updated => {
+                this.tasks[i] = updated;
+                this.sortTasks();
+              })
+              .catch(console.error)
+              .then(() => {
+                this.doneLoading();
+              })
+        }
+      })
+    },
+    sortTasks() {
+      this.tasks = this.tasks.sort((a, b) => {
+        if (a.done && !b.done) {
+          return 1;
+        } else if (!a.done && b.done) {
+          return -1;
+        }
+        if (a.id < b.id) {
+          return -1;
+        } else if (a.id > b.id) {
+          return 1;
+        }
+        return 0;
+      });
     }
   },
   computed: {
@@ -141,12 +170,23 @@ export default {
 .row > *:nth-child(1) {
   text-align: right;
 }
+
 .row > *:nth-child(2), .row > *:nth-child(3) {
   text-overflow: ellipsis;
   overflow: hidden;
 }
+
 .row > *:nth-child(4) {
   text-align: center;
+  cursor: pointer;
+}
+
+.row:not(.header) > *:nth-child(4).done:hover {
+  color: var(--fg-danger);
+}
+
+.row:not(.header) > *:nth-child(4):hover {
+  color: var(--fg-happy);
 }
 
 .row:nth-child(even) {
@@ -167,5 +207,10 @@ export default {
 
 .row.empty > * {
   text-align: center;
+}
+
+.row *.done {
+  text-decoration: line-through;
+  color: gray;
 }
 </style>
