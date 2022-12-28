814ee3d7dc95c091416372248e9a25b708c62a57
Doug Saylor <doug@saylorsolutions.com>
2022-12-28 02:53:38 -0600

    Splitting out task like I did with time - glanceability

--- data/data.go
+++ data/data.go
@@ -1,15 +1,2 @@
+// Package data provides base types and structure for data interactions.
 package data
-
-type Task struct {
-	ID          uint64 `json:"id" boltholdKey:"ID"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Done        bool   `json:"done" boltholdIndex:"Done"`
-	Priority    int    `json:"priority" boltholdIndex:"Priority"`
-	Favorite    bool   `json:"favorite"`
-	SoftDeleted bool   `json:"inactivated" boltholdIndex:"SoftDeleted"`
-}
-
-type exportModel struct {
-	Tasks []Task `json:"tasks"`
-}
---
+++ data/tasks.go
@@ -0,0 +1,15 @@
+package data
+
+type Task struct {
+	ID          uint64 `json:"id" boltholdKey:"ID"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Done        bool   `json:"done" boltholdIndex:"Done"`
+	Priority    int    `json:"priority" boltholdIndex:"Priority"`
+	Favorite    bool   `json:"favorite"`
+	SoftDeleted bool   `json:"inactivated" boltholdIndex:"SoftDeleted"`
+}
+
+type exportModel struct {
+	Tasks []Task `json:"tasks"`
+}
